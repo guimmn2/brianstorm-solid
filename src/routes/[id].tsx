baabe2a145c0ae78a67aa01ce8fd51fbac047510
Guilherme Nunes <gmnss1917@gmail.com>
2023-06-22 22:58:59 +0100

    realtime update a funcionar, mas dá um erro qualquer quando dá load na func componente

--- src/routes/[id].tsx
+++ src/routes/[id].tsx
@@ -1,8 +1,36 @@
-import { useParams } from "solid-start"
+import { createSignal } from "solid-js"
+import { createRouteAction, createRouteData, refetchRouteData, useParams } from "solid-start"
+import { pb } from "~/pocketbase"
+
+type Suggestion = {
+    id: string,
+    description: string,
+    votes: number
+}
 
 export default function BrainstormPage() {
     const params = useParams()
+    const suggestions = createRouteData(async () => {
+        const records = await pb.collection('suggestions').getFullList({
+            filter: `brainstormId = "${params.id}"`
+        })
+        let suggestions: Suggestion[] = []
+        records.forEach(r => {
+            suggestions.push({
+                id: r.id,
+                description: r.suggestion,
+                votes: r.vote
+            })
+        })
+        return suggestions
+    })
+    
+    pb.collection('suggestions').subscribe('*', () => refetchRouteData())
+    
     return (
+        <>
         <h1>Page {params.id}</h1>
+        <pre>{JSON.stringify(suggestions())}</pre>
+        </>
     )
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,13 @@
-import { Title, createRouteAction, redirect } from "solid-start";
+import { Title, createRouteAction } from "solid-start";
 import { pb } from "~/pocketbase";
 
 export default function Home() {
   const [subject, setSubject] = createRouteAction(async (form: FormData) => {
     const subject = form.get("subject") as string
     const response = await pb.collection('brainstorms').create({ title: subject })
-    console.log(response.id)
     if (response) {
-      redirect(`${response.id}`)
+      //redirect from solid-start was not working form some reason
+      window.location.replace(`${response.id}`)
     }
     else {
       console.error("something went wrong")
@@ -19,11 +19,11 @@ export default function Home() {
       <Title>Brianstorm</Title>
       <h1 class="text-5xl">What do you wish to Brainstorm ?</h1>
       <setSubject.Form>
-        <input 
-        class="text-5xl"
-        type="text" 
-        name="subject" 
-        placeholder="coolest waterparks to visit..."
+        <input
+          class="text-5xl"
+          type="text"
+          name="subject"
+          placeholder="coolest waterparks to visit..."
         />
       </setSubject.Form>
     </main>
