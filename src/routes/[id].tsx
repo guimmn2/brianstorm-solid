c7b915ba096665b838e3e650f5c8a77467996492
Guilherme Nunes <gmnss1917@gmail.com>
2023-06-23 23:28:21 +0100

    realtime voting is working

--- src/routes/[id].tsx
+++ src/routes/[id].tsx
@@ -1,5 +1,5 @@
-import { For, createResource, createSignal } from "solid-js"
-import { RouteDataArgs, createRouteAction, createRouteData, refetchRouteData, useParams, useRouteData } from "solid-start"
+import { For, createResource, onMount } from "solid-js"
+import { RouteDataArgs, createRouteAction, createRouteData, useParams, useRouteData } from "solid-start"
 import { pb } from "~/pocketbase"
 
 type Suggestion = {
@@ -8,65 +8,68 @@ type Suggestion = {
     votes: number
 }
 
-export function routeData({ params }: RouteDataArgs) {
-    return createRouteData(async (key) => {
-        console.log(key)
-        const records = await pb.collection('suggestions').getFullList({
-            filter: `brainstormId = "${params.id}"`
-        })
-        let suggestions: Suggestion[] = []
-        records.forEach(r => {
-            suggestions.push({
-                id: r.id,
-                description: r.suggestion,
-                votes: r.votes
-            })
-        })
-        return suggestions
-    }, { key: ['suggestions'] })
-}
-
 export default function BrainstormPage() {
     const params = useParams()
-    const suggestions = useRouteData<typeof routeData>()
-    const [voting, vote] = createRouteAction(voteFn)
-    /*
-    const suggestions = createRouteData(async () => {
-        const records = await pb.collection('suggestions').getFullList({
+    const [suggestions, { mutate, refetch }] = createResource(async () => {
+        //fetch list
+        const response = await pb.collection('suggestions').getFullList({
             filter: `brainstormId = "${params.id}"`
         })
         let suggestions: Suggestion[] = []
-        records.forEach(r => {
+        response.forEach(rec => {
             suggestions.push({
-                id: r.id,
-                description: r.suggestion,
-                votes: r.votes
+                id: rec.id,
+                description: rec.suggestion,
+                votes: rec.votes
             })
         })
         return suggestions
     })
-    */
 
-    //this won't work on the server
-    //pb.collection('suggestions').subscribe('*', () => refetchRouteData())
+    //subscribe to chnages
+    //had to use onMount so the subscription only runs in the client
+    onMount(() => pb.collection('suggestions').subscribe('*', ({ action, record }) => {
+        if (record.brainstormId != params.id) {
+            return
+        }
+        if (action === "create") {
+            mutate([...suggestions()!, { description: record.suggestion, votes: record.votes, id: record.id } as Suggestion])
+        }
+        else if (action === "update") {
+            mutate(suggestions()!.map(item => {
+                if (item.id === record.id) {
+                    return {
+                        id: item.id,
+                        description: item.description,
+                        votes: record.votes
+                    } as Suggestion
+                }
+                return item
+            }))
+        }
+    }))
 
     return (
         <>
             <For each={suggestions()}>
-                {(sug) =>
-                    <>
-                        <p>{sug.description}</p>
-                        <p>{sug.votes}</p>
-                        <vote.Form>
-                            <input type="hidden" name="id" value={sug.id} />
-                            <input type="submit" value="vote" />
-                        </vote.Form>
-                    </>
+                {
+                    (sug) =>
+                        <>
+                            <p>{sug.description}</p>
+                            <p>{sug.votes}</p>
+                            <button onClick={async () => {
+                                const response = await pb.collection('suggestions').update(sug.id, {
+                                    suggestion: sug.description,
+                                    votes: sug.votes + 1,
+                                    brainstormId: params.id
+                                })
+                                if (response) {
+                                    console.log('vote with sucess')
+                                }
+                            }}>vote</button>
+                        </>
                 }
             </For>
         </>
     )
-}
-
-async function voteFn(form: FormData) {
 }
